6a1b1c20ba853fceccb3f2239690970c515b162a
jlfme <jlfgeek@gmail.com>
2016-01-17 20:37:56 +0800

    First commit utils package

---
+++ myspider/utils/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# _*_ encoding: utf-8 _*_
+# ---------------------------------------
+# Created by: Jlfme<jlfgeek@gmail.com>
+# Created on: 2016-01-17 20:32:18
+# ---------------------------------------
