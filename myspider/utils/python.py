00d9e8a89756b12000bd7701f69c8a8caf52b073
jlfme <jlfgeek@gmail.com>
2016-01-25 20:58:36 +0800

    Add walk_modules and other features

--- myspider/utils/python.py
+++ myspider/utils/python.py
@@ -6,6 +6,71 @@
 # ---------------------------------------
 
 
+from importlib import import_module
+from pkgutil import iter_modules
+
+
+def load_object(path):
+    """Load an object given its absolute object path, and return it.
+    object can be a class, function, variable or an instance.
+    """
+    try:
+        dot = path.rindex('.')
+    except ValueError:
+        raise ValueError("Error loading object '%s': not a full path" % path)
+    module, name = path[:dot], path[dot + 1:]
+    mod = import_module(module)
+    try:
+        obj = getattr(mod, name)
+    except AttributeError:
+        raise NameError("Module '%s' doesn't define any object named '%s'" % (module, name))
+    return obj
+
+
+def walk_modules(path):
+    """Loads a module and all its submodules from the given module path and
+    returns them. If *any* module throws an exception while importing, that
+    exception is thrown back.
+
+    For example: walk_modules('myspider.utils')
+    """
+
+    mods = []
+    mod = import_module(path)
+    mods.append(mod)
+    if hasattr(mod, '__path__'):
+        for _, subpath, ispkg in iter_modules(mod.__path__):
+            fullpath = path + '.' + subpath
+            if ispkg:
+                mods += walk_modules(fullpath)
+            else:
+                submod = import_module(fullpath)
+                mods.append(submod)
+    return mods
+
+
+def run_in_thread(func, *args, **kwargs):
+    """Run function in thread, return a Thread object
+
+    """
+    from threading import Thread
+    thread = Thread(target=func, args=args, kwargs=kwargs)
+    thread.daemon = True
+    thread.start()
+    return thread
+
+
+def run_in_subprocess(func, *args, **kwargs):
+    """Run function in subprocess, return a Process object
+
+    """
+    from multiprocessing import Process
+    process = Process(target=func, args=args, kwargs=kwargs)
+    process.daemon = True
+    process.start()
+    return process
+
+
 def to_unicode(text, encoding=None, errors='strict'):
     """Return the unicode representation of a bytes object `text`. If `text`
     is already an unicode object, return it as-is."""
