b7ac35f5a898722025ed977b783b92fa3ecc1d9a
jlfme <jlfgeek@gmail.com>
2016-02-19 22:53:18 +0800

    First commit http middlewares module

---
+++ myspider/myhttp/middlewares/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# _*_ encoding: utf-8 _*_
+# ---------------------------------------
+# Created by: Jlfme<jlfgeek@gmail.com>
+# Created on: 2016-02-15 22:49:18
+# ---------------------------------------
